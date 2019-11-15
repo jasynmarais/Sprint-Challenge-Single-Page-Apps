3e108100590e08e86e6020f6a44717e34d7a14fd
Jasyn Marais <jasynmarais@gmail.com>
2019-11-15 11:48:39 +0200

    Add search functionality.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,29 +2,56 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    axios.get('https://rick-api.herokuapp.com/api/')
+    axios.get('https://rickandmortyapi.com/api/character/')
 			.then(res => {
-				setCharacters(res.characters);
+				setCharacters(res.data.results);
 			})
 			.catch(error => console.error(error))
 		return () => {
-			console.log('cleanup')
+			setCharacters([]);
 		};
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
+  useEffect(() => {
+		if (search !== '') {
+			console.log("SEARCHING");
+			let _searchResults = characters.filter(char => (
+				char.name.search(search) !== -1 ||
+				char.gender.search(search) !== -1 ||
+				char.species.search(search) !== -1 ||
+				char.type.search(search) !== -1 ||
+				char.status.search(search) !== -1
+			));
+			setSearchResults(_searchResults);
+		} else {
+			setSearch('');
+			setSearchResults([]);
+		}
+		return () => {
+		};
+	}, [search])
+
+  const charList = (searchResults.length > 0 ? searchResults : characters);
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm
+				search={search}
+				setSearch={setSearch}
+			/>
+      <h2>Character List</h2>
       {
-        characters.map(char => <CharacterCard data={char} />)
+        charList.map(char => <CharacterCard key={char.id} data={char} />)
       }
     </section>
   );
