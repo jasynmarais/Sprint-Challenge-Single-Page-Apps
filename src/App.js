7b4f135ec9f8e67ee132ea24f25f0211f134cc7a
Jasyn Marais <jasynmarais@gmail.com>
2019-11-15 10:35:35 +0200

    Initial commit and request.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
 
 
 export default function App() {
+  const [characters, setCharacters] = useState([])
+  useEffect(() => {
+    axios.get('https://rick-api.herokuapp.com/api/')
+    .then(res => {
+      setCharacters(res.characters);
+    })
+    .catch(error => console.error(error))
+    return () => {
+      console.log('cleanup');
+    };
+  }, [])
   return (
     <main>
       <Header />
